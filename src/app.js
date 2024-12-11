35ab68f982537c8243912ee47f9c9745b818b2c0
Kartikey Tandon <tandonkartiey11@gmail.com>
2024-12-11 13:40:15 +0530

    added body parser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/database");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(bodyParser.json());
 app.use("/api/teachers", require("./routes/teacherRoutes"));
 
-module.exports = app;
+module.exports = app;   
